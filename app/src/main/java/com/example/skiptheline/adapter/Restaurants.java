4da11c6c03ba472ab8ec412432e5d160eec50dfa
shahzebmurad <shahzebmurad64@gmail.com>
2022-11-26 14:31:41 -0800

    Started on HomePage

---
+++ app/src/main/java/com/example/skiptheline/adapter/Restaurants.java
@@ -0,0 +1,28 @@
+package com.example.skiptheline.adapter;
+
+public class Restaurants {
+
+    String name;
+    Integer imageUrl;
+
+    public Restaurants(String name, Integer imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(Integer imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
